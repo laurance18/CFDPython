37a877210bf98e9b9693aa93ee577513c6e41541
Deniz Işık <denizisik2004@gmail.com>
2026-06-03 20:52:48 +0300

    Add 2D linear convection script

---
+++ laurance18/06_Step5.py
@@ -0,0 +1,43 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
+
+# Variables
+nx = 81
+ny = 81
+nt = 100
+c = 1
+dx = 2 / (nx - 1)
+dy = 2 / (ny - 1)
+sigma = .2
+dt = sigma * dx
+
+x = np.linspace(0, 2, nx)
+y = np.linspace(0, 2, ny)
+
+u = np.ones((ny, nx)) # BCs arlready applied (everywhere is 1)
+un = np.copy(u) # Copy of u
+
+# ICs
+
+u[int(.5 / dy):int(1 / dy + 1),int(.5 / dx):int(1 / dx + 1)] = 2
+
+# Iterate
+
+for n in range(nt + 1): # Loop across number of time steps
+    un = u.copy()
+    u[1:, 1:] = (un[1:, 1:] - (c * dt / dx * (un[1:, 1:] - un[1:, :-1])) -
+                              (c * dt / dy * (un[1:, 1:] - un[:-1, 1:])))
+    u[0, :] = 1
+    u[-1, :] = 1
+    u[:, 0] = 1
+    u[:, -1] = 1
+
+# Plot
+
+fig = plt.figure(figsize=(11, 7), dpi=100)
+ax = fig.add_subplot(111, projection='3d')                      
+X, Y = np.meshgrid(x, y)                            
+surf = ax.plot_surface(X, Y, u[:], cmap=cm.viridis)
+plt.show()
